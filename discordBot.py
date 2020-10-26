3b994158201da91c09af783d7fec5ea471298e17
Jill-Kapchan <jill.ingrid@gmail.com>
2020-10-25 18:49:49 -0700

    first

---
+++ discordBot.py
@@ -0,0 +1,63 @@
+# Work with Python 3.6
+import random
+import asyncio
+import aiohttp
+import json
+from discord import Game
+from discord.ext.commands import Bot
+
+BOT_PREFIX = ("?", "!")
+TOKEN = "NjgyNzIxNTU5NTY3MTM4ODI4.XlhIAA.K_6uiuRe0AnsAf5EbB0tXKVS4u4" # Get at discordapp.com/developers/applications/me
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+
+
+@client.command(name='almightyJill',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['almighty_jill'],
+                pass_context=True)
+async def almighty_Jill(context):
+    possible_responses = [
+        'Call me old fashioned but I was born and raised to serve Qwestt. I was destined to cook for him, bathe him, do his laundry, and wash his dishes. I only live to cater to his needs. I am his property and if he ever cheats on me thats my fault and he caught me slipping.'
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+
+
+@client.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name="simping for Qwestt"))
+    print("Logged in as " + client.user.name)
+
+
+@client.command()
+async def bitcoin():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+    async with aiohttp.ClientSession() as session:  # Async HTTP request
+        raw_response = await session.get(url)
+        response = await raw_response.text()
+        response = json.loads(response)
+        await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
+
+client.run(TOKEN)
